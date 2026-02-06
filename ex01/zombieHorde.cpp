b60edd16f36f847ae4571920254e00ec266acaed
Imran Barbouch <imranbarbouch@hotmail.fr>
2026-02-06 19:55:12 +0100

    zombieHorde done, manque main

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -1,6 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibarbouc <ibarbouc@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-02-06 18:50:21 by ibarbouc          #+#    #+#             */
+/*   Updated: 2026-02-06 18:50:21 by ibarbouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
 
 Zombie	*zombieHorde(int N, std::string name)
 {
-
+	if (N <= 0)
+		return (NULL);
+    int i = 0;
+	Zombie *horde = new Zombie[N];
+    while (i < N)
+    {
+        horde[i].setName(name);
+        i++;
+    }
+    return (horde);
 }
