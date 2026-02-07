bcbc61fd08a15d2e3916fbaddd6c69d4303df5ff
Imran Barbouch <imranbarbouch@hotmail.fr>
2026-02-07 02:22:54 +0100

    debut 03

---
+++ ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibarbouc <ibarbouc@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-02-07 00:40:58 by ibarbouc          #+#    #+#             */
+/*   Updated: 2026-02-07 00:40:58 by ibarbouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main ()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+
+    std::cout << "memory adress of the string variable : " << &str << std::endl;
+    std::cout << "memory adress held by stringPTR : " << stringPTR << std::endl;
+    std::cout << "memory adress held by stringREF : " << &stringREF << std::endl;
+
+    std::cout << "value of the string variable : " << str << std::endl;
+    std::cout << "value pointed to by stringPTR : " << *stringPTR << std::endl;
+    std::cout << "value pointed to by stringREF : " << stringREF << std::endl;   
+}
