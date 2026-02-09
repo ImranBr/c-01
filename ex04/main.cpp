aa4693efe87985cbb08d6c8bfc35dd0bae1c7da9
Imran Barbouch <imranbarbouch@hotmail.fr>
2026-02-09 22:29:39 +0100

    04/05 done

---
+++ ex04/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    std::ifstream infile;
+    std::ofstream outfile;
+    std::string filename;
+    std::string s1;
+    std::string s2;
+    std::string line;
+    size_t pos;
+
+    if (argc != 4)
+    {
+        std::cerr << "Usage: ./sed <filename> <s1> <s2>" << std::endl;
+        return 1;
+    }
+
+    filename = argv[1];
+    s1 = argv[2];
+    s2 = argv[3];
+
+    if (s1.empty())
+    {
+        std::cerr << "Error: s1 must not be empty" << std::endl;
+        return 1;
+    }
+
+    infile.open(filename.c_str());
+    if (!infile)
+    {
+        std::cerr << "Error: cannot open input file" << std::endl;
+        return 1;
+    }
+
+    outfile.open((filename + ".replace").c_str());
+    if (!outfile)
+    {
+        std::cerr << "Error: cannot create output file" << std::endl;
+        return 1;
+    }
+
+    while (std::getline(infile, line))
+    {
+        pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos)
+        {
+            line.erase(pos, s1.length());
+            line.insert(pos, s2);
+            pos += s2.length();
+        }
+        outfile << line;
+        if (!infile.eof())
+            outfile << std::endl;
+    }
+
+    infile.close();
+    outfile.close();
+    return 0;
+}
